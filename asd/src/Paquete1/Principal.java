954565657ceb56596548eeca3c7c2cc7bbcea6e7
cabralyo <cabralyo@DESKTOP-BROIGU6>
2023-03-21 23:42:11 +0100

    Otro cambio

---
+++ asd/src/Paquete1/Principal.java
@@ -0,0 +1,14 @@
+package Paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Estamos realizando un control de versiones con la herramienta Git");
+		System.out.println("Este mensaje es para continuar con la práctica y modificar el programa");
+		System.out.println("Este es otro cambio");
+
+		
+
+	}
+
+}
